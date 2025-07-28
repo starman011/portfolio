8399e3077a27d71bcb8ba0db4f0e68b29d7e08d9
Md Saqlain Khan <87300695+starman011@users.noreply.github.com>
2025-07-28 17:06:50 +0530

    Remove dynamic island component

--- js/components/dynamic-island.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const island = document.getElementById('about-island');
-  if (!island) return;
-  island.addEventListener('click', () => {
-    island.classList.toggle('open');
-  });
-});
